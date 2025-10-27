c1e975d77fc66a9fd02f187bb97f993829400816
KajetanKalinowski <125036724+KajetanKalinowski@users.noreply.github.com>
2025-10-27 18:02:23 +0100

    ajajaj

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ const [error,setError] = useState(false)
 useEffect(()=>{
     const getdata = async ()=>{
       try{
-      const data = await fetch(`https://playerdb.co/api/player/minecraft/HitchedHorse311`)
+      const data = await fetch(`https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all`)
       const json = await data.json()
       setFruit(json)
       setLadowanie(false)
@@ -21,9 +21,17 @@ useEffect(()=>{
     getdata()
   },[])
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    {error && "Wystąpił błąd podczas ładowania danych"}
+    {ladowanie?"Ładowanie danych...":(
+    <div className="App flex flex-row flex-wrap w-full">
+      {fruit.map((item,idx)=>(
+        <div key={idx} className='flex flex-col w-[50px] h-[50px]'>
+          <h1 className='flex text-xs w-[20px] max-w-[20px] h-[20px]'>{item.name}</h1>
+          </div>
+      ))}
+    </div>)}
+    </>
   );
 }
 
